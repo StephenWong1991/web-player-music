d96d151038ba8f4b1edf83265a28b4c362d7e71f
Wang Sai <532969033@qq.com>
2023-06-26 21:15:23 +0800

    add bar effect

--- src/components/AudioPlayer/index.tsx
+++ src/components/AudioPlayer/index.tsx
@@ -1,30 +1,47 @@
 import React, { useState, useRef, useEffect, Fragment } from "react";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
 import audioAnalyser from "./audioAnalyser";
 import { loadImage } from "../../utils";
-import musicUrl from "../../music/5458_560e_0e5c_c2b021013fadd328f32b968bd9f15cb6.m4a";
-import coverUrl from "../../music/109951163580419226.jpeg";
+import musicUrl from "../../music/C400003VLsik0ztbIb.mp4";
+import coverUrl from "../../music/T002R300x300M000002Neh8l0uciQZ_1.webp";
 
-// const musicUrl =
-//   "https://m701.music.126.net/20230626004502/4ee310eb6f9cf0bfe30ea81e84fc413b/jdyyaac/5458/560e/0e5c/c2b021013fadd328f32b968bd9f15cb6.m4a";
-// const coverUrl =
-//   "http://p2.music.126.net/e9NayWFI395VUeYantarWg==/109951163580419226.jpg?param=130y130";
+enum MusicEffect {
+  ARC = "arc-waveform",
+  BAR = "bar-waveform",
+}
 
+const effects = ["arc-waveform", "bar-waveform"];
 const radius = 450;
 const coverRadius = radius - 30;
 const rectWidth = 5;
 let rotation = 0; // 初始旋转角度
+
+const capYPositionArray: number[] = [];
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+  },
+}));
 
 const AudioPlayer: React.FC = () => {
   const [size, setSize] = useState({
     width: window.innerWidth,
     height: window.innerHeight,
   });
+  const [effect, setEffect] = useState<MusicEffect>(MusicEffect.ARC);
   const audioRef = useRef<HTMLAudioElement>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const ctxRef = useRef<CanvasRenderingContext2D | null>(null);
   const loopIdRef = useRef<number | null>(null);
   const isPlayingRef = useRef<boolean>(false);
   const coverRef = useRef<HTMLImageElement | null>(null);
+  const lastEffect = useRef<MusicEffect>(MusicEffect.ARC);
+  lastEffect.current = effect;
+  const classes = useStyles();
 
   useEffect(() => {
     const handleResize = () => {
@@ -41,69 +58,74 @@ const AudioPlayer: React.FC = () => {
     ctxRef.current = canvasRef.current?.getContext(
       "2d"
     ) as CanvasRenderingContext2D;
-    const draftArray = Array.from({ length: 256 }, () => 5);
+    const draftArray: Uint8Array = new Uint8Array(256);
     loadImage(coverUrl).then((img) => {
       coverRef.current = img as HTMLImageElement;
       if (!isPlayingRef.current) {
-        draw(draftArray);
+        renderCurrentTime(draftArray);
       }
     });
-    draw(draftArray);
+    renderCurrentTime(draftArray);
   }, []);
 
-  const draw = (datas: number[]) => {
-    const ctx = ctxRef.current;
-    if (!ctx) return;
+  useEffect(() => {
+    capYPositionArray.length = 0;
+  }, [effect]);
 
-    const centerX = ctx.canvas.width / 2;
-    const centerY = ctx.canvas.height / 2;
+  // draw bg
+  const renderBlurBg = () => {
+    const ctx = ctxRef.current;
+    if (!coverRef.current || !ctx) return;
+
+    const { width, height } = ctx.canvas;
+    const centerX = width / 2;
+    const centerY = height / 2;
+    ctx.save();
+    ctx.fillStyle = "#ffffff00";
+    ctx.filter = "blur(80px)";
+    ctx.fillRect(0, 0, width, height);
+    ctx.drawImage(coverRef.current, -centerX, -centerY, width * 2, height * 2);
+    ctx.restore();
+  };
+
+  // draw cover
+  const renderCover = () => {
+    const ctx = ctxRef.current;
+    if (!coverRef.current || !ctx) return;
+
+    const { width, height } = ctx.canvas;
+    const centerX = width / 2;
+    const centerY = height / 2;
+    ctx.save();
+    ctx.beginPath();
+    ctx.arc(centerX, centerY, coverRadius, 0, 2 * Math.PI);
+    ctx.closePath();
+    ctx.clip();
+    ctx.translate(centerX, centerY);
+    ctx.rotate(rotation);
+    ctx.translate(-centerX, -centerY);
+    ctx.drawImage(
+      coverRef.current,
+      centerX - coverRadius,
+      centerY - coverRadius,
+      coverRadius * 2,
+      coverRadius * 2
+    );
+    ctx.restore();
+    // 更新旋转角度
+    rotation += 0.01;
+  };
+
+  const renderArcWaveform = (
+    ctx: CanvasRenderingContext2D,
+    datas: number[]
+  ) => {
+    const { width, height } = ctx.canvas;
+    const centerX = width / 2;
+    const centerY = height / 2;
     const startAngle = Math.PI; // 270 度
     const rectCount = datas.length;
-
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-
-    if (coverRef.current) {
-      ctx.save();
-      ctx.fillStyle = "#ffffff00";
-      ctx.filter = "blur(80px)";
-      ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-      ctx.drawImage(
-        coverRef.current,
-        -ctx.canvas.width / 2,
-        -ctx.canvas.height / 2,
-        ctx.canvas.width * 2,
-        ctx.canvas.height * 2
-      );
-      ctx.restore();
-
-      ctx.save();
-      ctx.beginPath();
-      ctx.arc(
-        ctx.canvas.width / 2,
-        ctx.canvas.height / 2,
-        coverRadius,
-        0,
-        2 * Math.PI
-      );
-      ctx.closePath();
-      ctx.clip();
-      ctx.translate(centerX, centerY);
-      ctx.rotate(rotation);
-      ctx.translate(-centerX, -centerY);
-      ctx.drawImage(
-        coverRef.current,
-        ctx.canvas.width / 2 - coverRadius,
-        ctx.canvas.height / 2 - coverRadius,
-        coverRadius * 2,
-        coverRadius * 2
-      );
-      ctx.restore();
-      // 更新旋转角度
-      rotation += 0.01;
-    }
-
     ctx.strokeStyle = ctx.fillStyle = "#ec9446";
-
     ctx.beginPath();
     for (let i = 0; i < rectCount; i++) {
       const rectHeight = Math.max(datas[i] * 0.5, 5);
@@ -123,22 +145,112 @@ const AudioPlayer: React.FC = () => {
     ctx.fill();
   };
 
-  const update = () => {
+  const renderBarWaveform = (
+    ctx: CanvasRenderingContext2D,
+    datas: number[]
+  ) => {
+    const marginX = 200;
+    const marginY = 400;
+    const { width, height } = ctx.canvas;
+    const xStart = marginX;
+    const yStart = height - marginY;
+    const xEnd = width - marginX;
+    const yEnd = marginY;
+
+    const meterWidth = 24;
+    const gap = 4;
+    const meterNum = (width - marginX * 2) / (meterWidth + gap);
+    const step = Math.round(datas.length / meterNum);
+    const capHeight = 5;
+    const capStyle = "#fff";
+    const gradient = ctx.createLinearGradient(
+      xStart,
+      yEnd,
+      (width / 2 - marginX) * window.devicePixelRatio,
+      (height / 2 - marginY) * window.devicePixelRatio
+    );
+    gradient.addColorStop(1, "#0f0");
+    gradient.addColorStop(0.5, "#ff0");
+    gradient.addColorStop(0, "#f00");
+
+    ctx.beginPath();
+
+    for (var i = 0; i < meterNum; i++) {
+      const value = Math.max(datas[i * step] * 3, 10);
+
+      if (capYPositionArray.length < Math.round(meterNum)) {
+        capYPositionArray.push(value);
+      }
+      // ctx.fillStyle = capStyle;
+      ctx.fillStyle = gradient;
+      if (value < capYPositionArray[i]) {
+        ctx.fillRect(
+          xStart + i * (meterWidth + gap),
+          yStart - --capYPositionArray[i],
+          meterWidth,
+          capHeight
+        );
+      } else {
+        ctx.fillRect(
+          xStart + i * (meterWidth + gap),
+          yStart - value,
+          meterWidth,
+          capHeight
+        );
+        capYPositionArray[i] = value;
+      }
+
+      ctx.fillStyle = gradient;
+      ctx.roundRect(
+        xStart + i * (meterWidth + gap),
+        yStart - value + capHeight,
+        meterWidth,
+        value,
+        2
+      );
+    }
+    ctx.fill();
+  };
+
+  const renderCurrentTime = (datas: Uint8Array) => {
+    const ctx = ctxRef.current;
+    if (!ctx) return;
+
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+    renderBlurBg();
+
+    const len = datas.length;
+
+    if (lastEffect.current === MusicEffect.ARC) {
+      renderCover();
+      const offset = Math.floor((len * 2) / 3);
+      const waveformList = new Array(offset * 2);
+      for (let i = 0; i < offset; i++) {
+        waveformList[i] = waveformList[waveformList.length - i - 1] = datas[i];
+      }
+      renderArcWaveform(ctx, waveformList);
+    }
+
+    if (lastEffect.current === MusicEffect.BAR) {
+      const offset = Math.floor(len * 2);
+      const waveformList = new Array(offset);
+      for (let i = 0; i < offset; i++) {
+        waveformList[i] = i < len ? datas[len - 1 - i] : datas[i - len];
+      }
+      renderBarWaveform(ctx, waveformList);
+    }
+  };
+
+  const loopEffect = () => {
     if (!isPlayingRef.current) {
       window.cancelAnimationFrame(loopIdRef.current!);
       return;
     }
     audioAnalyser.analyser.getByteFrequencyData(audioAnalyser.buffer);
 
-    const offset = Math.floor((audioAnalyser.buffer.length * 2) / 3);
-    const datas = new Array(offset * 2);
-    for (let i = 0; i < offset; i++) {
-      datas[i] = datas[datas.length - i - 1] = audioAnalyser.buffer[i];
-    }
-
-    // draw(audioAnalyser.buffer);
-    draw(datas);
-    loopIdRef.current = window.requestAnimationFrame(update);
+    renderCurrentTime(audioAnalyser.buffer);
+    loopIdRef.current = window.requestAnimationFrame(loopEffect);
   };
 
   const handleLoadedMetadata = () => {
@@ -149,7 +261,7 @@ const AudioPlayer: React.FC = () => {
 
   const handleOnPlay = () => {
     isPlayingRef.current = true;
-    update();
+    loopEffect();
   };
 
   const handleOnPause = () => {
@@ -158,6 +270,18 @@ const AudioPlayer: React.FC = () => {
 
   return (
     <Fragment>
+      <div className={`effects ${classes.root}`}>
+        {effects.map((v, i) => (
+          <Button
+            key={i}
+            variant="contained"
+            color="primary"
+            onClick={() => setEffect(v as MusicEffect)}
+          >
+            {v}
+          </Button>
+        ))}
+      </div>
       <canvas
         ref={canvasRef}
         width={size.width * window.devicePixelRatio}
@@ -166,9 +290,11 @@ const AudioPlayer: React.FC = () => {
       />
       <audio
         controls
+        preload="auto"
         crossOrigin="anonymous"
         ref={audioRef}
         onLoadedMetadata={handleLoadedMetadata}
+        onCanPlayThrough={() => console.log("canplay")}
         onPlay={handleOnPlay}
         onSeeking={() => console.log("loading...")}
         onSeeked={() => console.log("loaded")}
--- src/module-name.d.ts
+++ src/module-name.d.ts
@@ -1,3 +1,5 @@
 declare module "*.jpeg";
+declare module "*.webp";
 declare module "*.mp3";
 declare module "*.m4a";
+declare module "*.mp4";
